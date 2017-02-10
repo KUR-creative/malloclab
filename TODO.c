cb72ceff7a4ead13968a6c565941df7424abbc6c
KUR <rhdnfka94@gmail.com>
2017-02-10 20:51:35 +0900

    added: TODO

---
+++ TODO.c
@@ -0,0 +1,23 @@
+objective: high score malloc/free
+
+impl free list -> expl flist -> segregated flist -> more optimzation
+
+more: pro-like development!
+	use git to version control!
+	pro-like testing
+	consistent naming convention
+		parameter, local variables use snake_case, 
+		and short words and abbreviation
+
+		more permanant thing, use carmelCase.
+		function name, struct member name.
+
+*:
+	separate test file mm-test.c
+
+	make my own general purpose test/debug package: kur-test.h, kur-test.c
+		TEST(suite,case) cmocka function wrapper macro
+		on/off kprint(color, flaglist, ...) function macro(colored)
+			you can on/off to set the flaglist(global var)
+
+1. implicit free list
