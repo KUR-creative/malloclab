6738814f1d2e1030377d39b9b80b9ff2b434854d
KUR <rhdnfka94@gmail.com>
2017-02-13 16:43:33 +0900

    added: printer.py print skip msg in yellow.

--- printer.py
+++ printer.py
@@ -4,6 +4,8 @@ import fileinput
 RED = '\033[0;31m'
 GREEN = '\033[0;32m'
 LGREEN = '\033[1;32m'
+YELLOW = '\033[0;33m'
+LYELLOW = '\033[1;33m'
 CYAN = '\033[0;36m'
 LCYAN = '\033[1;36m'
 NOCOL = '\033[0m'
@@ -22,6 +24,8 @@ for line in fileinput.input():
         headColorPrinter(line, LGREEN)
     elif isStrInLine(line, '[==========]'):
         headColorPrinter(line, CYAN)
+    elif isStrInLine(line, '[  SKIPPED ]'):
+        headColorPrinter(line, LYELLOW)
     else:
         subprocess.call(['printf', NOCOL + line])
     pass
